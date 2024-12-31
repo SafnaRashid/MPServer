d5fc9337003a1a2aaecccbee00e3410e2f8cf8ec
SafnaRashid <safna3051999@gmail.com>
2024-12-31 12:13:49 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const jsonserver=require("json-server")
+const MPServer=jsonserver.create()
+
+const middleware=jsonserver.defaults()
+const PORT=3000
+
+const route=jsonserver.router("db.json")
+
+MPServer.use(middleware)
+MPServer.use(route)
+
+MPServer.listen(PORT,()=>{
+    console.log(`MPServer started running at port :${PORT} and its waiting for client request`);
+    
+})
